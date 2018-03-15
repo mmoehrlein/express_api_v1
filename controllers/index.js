b41a81edcc604cebd71192b38977757daa944826
Michael Moehrlein <michael.moehrlein@picturemaxx.com>
2018-03-15 17:29:36 +0100

    setting up the basic structures and auth0

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.use('/users', require('./usersController'));
+router.use('/dormip', require('./dormIpController'));
+router.use('/slides', require('./slidesController'));
+
+router.get('/', function(req, res){
+    res.json({"message":"api index"});
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+const jwtAuthz = require('express-jwt-authz');
+
+const checkJwt = require("middlewares/authMiddleware.js");
+const checkScopes = jwtAuthz(['read:messages']);
+
+
+// loading environment variables
+require('dotenv')
+    .config();
+
+// using middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// serving static content
+app.use(express.static(__dirname + '/public'));
+
+// routing for api
+app.use('/api/' + process.env.API_VERSION, require('./controllers'));
+
+app.get('/', function(req, res){
+    res.json({"message":"working"});
+});
+
+app.get('/public_test', function(req, res){
+    res.json({"message":"public test working"});
+});
+
+app.get('/private_test', checkJwt, function(req, res){
+    res.json({"message":"private test working"});
+});
+
+app.get('/private_scope_test', checkJwt, checkScopes, function(req, res){
+    res.json({"message":"private scope test working"});
+});
+
+var port = process.env.PORT || 3000;
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port, function(){
+    console.log('Listening on port ' + port + '...');
+});
